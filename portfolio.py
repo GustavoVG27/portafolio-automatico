9cf111bc27020097a3d05be99c3e60389314953e
GustavoVG27 <gustavovalderaeguiluz@gmail.com>
2026-01-23 16:17:26 -0500

    Add files via upload

---
+++ portfolio.py
@@ -0,0 +1,72 @@
+from datetime import date
+
+PORTAFOLIO = {
+    "NVDA": {
+        "nombre": "NVIDIA Corp.",
+        "tipo": "Acción",
+        "fecha_compra": date(2025, 12, 29),
+        "cantidad": 0.53573,
+        "precio_compra": 186.66,
+        "invertido": 100.00
+    },
+    "TSLA": {
+        "nombre": "Tesla Inc.",
+        "tipo": "Acción",
+        "fecha_compra": date(2025, 12, 29),
+        "cantidad": 0.03543,
+        "precio_compra": 467.10,
+        "invertido": 16.55
+    },
+    "UBER": {
+        "nombre": "Uber Technologies",
+        "tipo": "Acción",
+        "fecha_compra": date(2025, 12, 29),
+        "cantidad": 0.18434,
+        "precio_compra": 81.37,
+        "invertido": 15.00
+    },
+    "PANW": {
+        "nombre": "Palo Alto Networks",
+        "tipo": "Acción",
+        "fecha_compra": date(2025, 12, 29),
+        "cantidad": 0.10628,
+        "precio_compra": 186.76,
+        "invertido": 19.85
+    },
+
+    # ETF S&P 500 UCITS (Irlanda / Londres)
+    "CSPX.L": {
+        "nombre": "iShares Core S&P 500 UCITS ETF",
+        "tipo": "ETF",
+        "fecha_compra": date(2025, 12, 29),
+        "cantidad": 0.29640,
+        "precio_compra": 741.41,
+        "invertido": 219.74
+    },
+
+    "JEPI": {
+        "nombre": "JPMorgan Equity Premium Income ETF",
+        "tipo": "ETF",
+        "fecha_compra": date(2026, 1, 20),
+        "cantidad": 0.58726,
+        "precio_compra": 57.64,
+        "invertido": 33.85
+    },
+    "VHT": {
+        "nombre": "Vanguard Health Care ETF",
+        "tipo": "ETF",
+        "fecha_compra": date(2026, 1, 21),
+        "cantidad": 0.11705,
+        "precio_compra": 289.18,
+        "invertido": 33.85
+    },
+    "URA": {
+        "nombre": "Global X Uranium ETF",
+        "tipo": "ETF",
+        "fecha_compra": date(2026, 1, 20),
+        "cantidad": 0.52935,
+        "precio_compra": 54.50,
+        "invertido": 28.85
+    }
+}
+
